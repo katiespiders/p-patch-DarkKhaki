cca837d525a2b61978819342681d2922d3bd3990
Cate Uselton <cathode.use@gmail.com>
2014-12-03 13:45:44 -0800

    added shared item tests for quantities less than 0

--- spec/models/shared_item_spec.rb
+++ spec/models/shared_item_spec.rb
@@ -1,22 +1,35 @@
 require 'rails_helper'
 describe SharedItem do
   describe '#self.create_many' do
-    
-    it "creates SharedItem db entries with the given quantity and name" do
-      SharedItem.create_many('hats',3)
-      expect(SharedItem.all.count).to eq(3)
+
+    context "non-zero quantity and a name" do
+      it "creates SharedItem db entries with the given quantity and name" do
+        SharedItem.create_many('hats',3)
+        expect(SharedItem.all.count).to eq(3)
+      end
     end
 
-    it "does not create items when they are not valid" do
-      SharedItem.create_many('',3)
-      expect(SharedItem.all.count).to eq 0
+    context "no name given" do
+      it "does not create items when they are not valid" do
+        SharedItem.create_many('',3)
+        expect(SharedItem.all.count).to eq 0
+      end
+
+      it "returns a SharedItem object with errors" do
+        item = SharedItem.create_many('',2)
+        expect(item).to be_instance_of SharedItem
+        expect(item.errors.any?).to eq(true)
+      end
     end
 
-    it "returns a SharedItem object with errors" do
-      item = SharedItem.create_many('',2)
-      expect(item).to be_instance_of SharedItem
-      expect(item.errors.any?).to eq(true)
+    context "given a quantity equal or less than 0" do
+      it "returns a SharedItem object with erros" do
+        item = SharedItem.create_many('hats', 0)
+        expect(item).to be_instance_of SharedItem
+        expect(item.errors.any?).to eq(true)
+      end
     end
+
 
   end
 
