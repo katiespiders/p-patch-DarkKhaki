d2b2a1a644333fc3f780f98ebfb06ec8a18ff2ed
Katie <kab761@gmail.com>
2014-12-03 11:56:00 -0800

    added default value of false to admin column for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202230814) do
+ActiveRecord::Schema.define(version: 20141203191946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20141202230814) do
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
