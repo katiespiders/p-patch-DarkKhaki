02ab965df3852719facfcc99e0af049b051637f9
Cate Uselton <cathode.use@gmail.com>
2014-12-09 10:30:30 -0800

    calendar class makes more days

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,6 +4,27 @@ module EventsHelper
     Day.new(date).box
   end
 
+  def make_month(date)
+  Calendar.new(date.month, date.year).make_days
+  end
+
+  class Calendar
+
+    def initialize(month, year)
+      @month = month
+      @year = year
+      @day_num = Time.days_in_month(@month, year)
+    end
+
+    def make_days
+      html = (1..@day_num).collect do |day|
+        Day.new(Date.parse("#{@year}-#{@month}-#{day}")).box
+      end
+      html.join('').html_safe
+    end
+
+  end
+
   class Day
 
     def initialize(date)
