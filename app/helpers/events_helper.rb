b4c4467aea858aca56f3410d65cd4edcd6e52496
Cate Uselton <cathode.use@gmail.com>
2014-12-09 11:15:06 -0800

    calendar makes display that is day of the week ignorant

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,11 +4,15 @@ module EventsHelper
     Day.new(date).box
   end
 
-  def make_month(date)
-  Calendar.new(date.month, date.year).make_days
+  # def make_month(date)
+  #   Calendar.new(date.month, date.year).make_days
+  # end
+
+  def display_calendar(date)
+    Cal.new(date.month, date.year).table
   end
 
-  class Calendar
+  class Cal
 
     def initialize(month, year)
       @month = month
@@ -17,10 +21,49 @@ module EventsHelper
     end
 
     def make_days
-      html = (1..@day_num).collect do |day|
+      (1..@day_num).collect do |day|
         Day.new(Date.parse("#{@year}-#{@month}-#{day}")).box
       end
-      html.join('').html_safe
+    end
+
+    def make_week(day_array, week_num)
+      day_num = (week_num-1) *7
+      # day_array[day_num, day_num+6].join('').html_safe
+      array_to_html(day_array[day_num..day_num+6])
+    end
+
+    def make_weeks(day_array)
+      week_array= (1..5).collect do |week_num|
+        "<tr>" + make_week(day_array, week_num) + "</tr>"
+      end
+      array_to_html(week_array)
+    end
+
+    def  array_to_html(array)
+      array.join('').html_safe
+    end
+
+    def table
+      """
+        <table>
+          <tr> <th colspan='7'> #{Date::MONTHNAMES[@month]} </th> </tr>
+          <tr> #{days_of_week} </tr>
+          #{make_weeks(make_days)}
+
+        </table>
+      """.html_safe
+    end
+
+    def days_of_week
+      days = Date::ABBR_DAYNAMES
+      day_array = days.collect do |day|
+        """
+          <th>
+            #{day}
+          </th>
+        """
+      end
+      array_to_html(day_array)
     end
 
   end
@@ -33,9 +76,11 @@ module EventsHelper
 
     def box
       html = """
-      <div class= \"day_box\">
-        #{@date.day}
-      </div>
+      <td>
+        <div class= \"day_box\">
+          #{@date.day}
+        </div>
+      </td>
       """
       html.html_safe
     end
