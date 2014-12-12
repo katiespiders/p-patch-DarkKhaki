38e3457cd2c8fcc40971b174b81169e9e2d55249
Cate Uselton <cathode.use@gmail.com>
2014-12-12 14:27:29 -0800

    about is not landing for not logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
+
+  def landing
+    if current_user
+      redirect_to :articles
+    else
+      redirect_to :about
+    end
+  end
+
   def about
-    render layout: false
+    render 'shared/about', layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ require 'resque/server'
 Rails.application.routes.draw do
   mount Resque::Server, at: '/resque'
 
-  root  'articles#index'
+  root  'pages#landing'
+
+  get 'about',               to: 'pages#about',       as: :about
 
 ######### SESSIONS ROUTES
   get     'sessions/new',     to: 'sessions#new',     as: :sessions
@@ -19,14 +21,14 @@ Rails.application.routes.draw do
   patch   'users/:id/',       to: 'users#update'
   delete  'users/:id',        to: 'users#destroy'
 
-  get     '/admin',           to: 'users#admin',      as: :admin
-  patch   '/admin',           to: 'users#make_admin'
+  get     'admin',           to: 'users#admin',      as: :admin
+  patch   'admin',           to: 'users#make_admin'
 
 ######### SHARED ITEMS ROUTES
-  get '/library',             to: 'shared_items#index',     as: :library
-  post '/library/checkout',   to: 'shared_items#checkout',  as: :ajax_checkout
-  post '/library',            to: 'shared_items#create'
-  patch '/library',           to: 'shared_items#checkout'
+  get 'library',             to: 'shared_items#index',     as: :library
+  post 'library/checkout',   to: 'shared_items#checkout',  as: :ajax_checkout
+  post 'library',            to: 'shared_items#create'
+  patch 'library',           to: 'shared_items#checkout'
 
 ######### ARTICLES ROUTES
   get     'articles',         to: 'articles#index',   as: :articles
